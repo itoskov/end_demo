73203294521afa0700324ed424a9a35b56fcf4d1
itoskov <itoskov@gmail.com>
2020-03-04 20:23:42 +0100

    mpj prvi commit

---
+++ src/main/java/com/poly/tole/ToleApplication.java
@@ -0,0 +1,13 @@
+package com.poly.tole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poly/tole/ToleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.tole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
