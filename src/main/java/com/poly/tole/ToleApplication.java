a03b3cdac195e8e13630f40c4f08f5b89469c937
itoskov <itoskov@gmail.com>
2020-03-04 20:25:39 +0100

    dodat log

--- src/main/java/com/poly/tole/ToleApplication.java
+++ src/main/java/com/poly/tole/ToleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ToleApplication {
 
 	public static void main(String[] args) {
+		//some comment
 		SpringApplication.run(ToleApplication.class, args);
 	}
 
